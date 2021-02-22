c6aa4db5d44f5083b6d3f638e52a1a823d701b02
Andy Mina <andy.mina@macaulay.cuny.edu>
2021-02-21 20:39:11 -0500

    Added myFilter

--- group-1.js
+++ group-1.js
@@ -24,3 +24,28 @@ Array.prototype.myMap = function (callback) {
     // return the new array
     return res;
 }
+
+/**
+ * myFilter creates a new array with only elements
+ * that pass the test provided in the callback
+ * 
+ * @param callback A function to be executed on every element.
+ * Takes three parameters:
+ *      currentValue - the current element being processed
+ *      index - the current index being processed
+ *      array - the array this function was called on
+ * @returns A new array with only the elements that pass the test.
+ */
+Array.prototype.myFilter = function (callback) {
+    // create res array
+    let res = [];
+    // loop through every element
+    for (let i = 0; i < this.length; i++) {
+        // if the element passes the callback
+        // add to res array
+        if (callback(this[i], i, this))
+            res.push(this[i]);
+    }
+    // return the result
+    return res;
+}
