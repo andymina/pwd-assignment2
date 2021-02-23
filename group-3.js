af5cc6d0499d4f661c799acf79414c2c3147b621
Hashir Khan <hkhan6265@bths.edu>
2021-02-22 22:12:52 -0500

    indexof completed

--- group-3.js
+++ group-3.js
@@ -1,1 +1,17 @@
 // Hashir Khan
+
+// indexOf()
+// Without using the native “Array.prototype.indexOf”
+// method of JavaScript, compose a function titled
+// “myIndexOf” that will take in an array of elements
+// and returns the index of the first encounter of a
+// target element (if it is found) or -1 if that
+// element does not exist within the input array.
+
+Array.prototype.indexOf = function(value) {
+  for (var i = 0; i < this.length; i++) {
+    if (arr[i] == value)
+      return i;
+  }
+  return -1;
+}
