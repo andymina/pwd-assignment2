7989670384f19e1a1b975491ce0e6c5d37e439a7
Stacey Li <staceyli@Staceys-MacBook-Pro.local>
2021-02-23 22:56:10 -0500

    added and cleaned up documentation

--- group-2.js
+++ group-2.js
@@ -1,10 +1,16 @@
 // Stacey Li
-//
-// Object.keys()
-//
-// Without using the native “Object.keys()” method of JavaScript,
-// compose a function titled “grabKeys” that will take in an object and
-// return all of the keys of the key:value pairs of that object.
+
+/**
+Object.keys()
+
+Without using the native “Object.keys()” method of JavaScript,
+compose a function titled “grabKeys” that will take in an object and
+return all of the keys of the key:value pairs of that object.
+
+@params takes in an object
+@returns all the keys in the object
+**/
+
 function grabKeys(obj){
   let keyArray = [];
   for (let key in obj){
@@ -13,30 +19,40 @@ function grabKeys(obj){
   return keyArray;
 }
 
-// Object.values()
-//
-// Without using the native “Object.values()” method of JavaScript,
-// compose a function titled “grabValues” that will take in an object and
-// return all of the values of the key:value pairs of that object.
+/**
+Object.values()
+
+Without using the native “Object.values()” method of JavaScript,
+compose a function titled “grabValues” that will take in an object and
+return all of the values of the key:value pairs of that object.
+
+@params takes in an object
+@returns all the values in the object
+**/
 
 function grabValues(obj){
-  let keyArray = [];
+  let valArray = [];
   for (let key in obj){
-    keyArray.push(obj[key]);
+    valArray.push(obj[key]);
   }
-  return keyArray;
+  return valArray;
 }
 
-// push()
-//
-// Without using the native “Array.prototype.push” method of JavaScript,
-// compose a function titled “myPush” that will take in an array of elements
-// as well as an elementToAdd and append that element to the end of the array.
+/**
+push()
+
+Without using the native “Array.prototype.push” method of JavaScript,
+compose a function titled “myPush” that will take in an array of elements
+as well as an elementToAdd and append that element to the end of the array.
+
+@params takes in an array of elements and an element to add
+**/
 
 function myPush(arr, elementToAdd){
   arr[arr.length] = elementToAdd
 }
 
+// TESTING
 let person = {
   firstName: "John",
   lastName: "Doe",
@@ -45,17 +61,21 @@ let person = {
 };
 
 const animals = ['pigs', 'goats', 'sheep'];
-// animals.push("cow");
+const animals1 = ['pigs', 'goats', 'sheep'];
+
 myPush(animals, "cow");
 myPush(animals, "tree");
 
-const animals1 = ['pigs', 'goats', 'sheep'];
+/* native func */
 animals1.push("cow");
 animals1.push("tree");
 
-console.log(Object.keys(person));
+console.log("Testing grabKeys");
+console.log(Object.keys(person));   /* native func */
 console.log(grabKeys(person));
-console.log(Object.values(person));
+console.log("Testing grabValues");
+console.log(Object.values(person)); /* native func */
 console.log(grabValues(person));
-console.log(animals1);
+console.log("Testing myPush");
+console.log(animals1);              /* native func */
 console.log(animals);
